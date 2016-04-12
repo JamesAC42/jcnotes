1467f5abce4579d5f20d2b3580f4c64b71dff5c7
James Crovo <jamescrovo2019@lschs.org>
2016-04-12 18:04:00 -0400

    Finish roughdraft

--- Tools/wordcounter/wordcounter.js
+++ Tools/wordcounter/wordcounter.js
@@ -1,6 +1,22 @@
-var lower = "abcdefghijklmnopqrstuvwxyz";
+var lower = "abcdefghijklmnopqarstuvwxyz";
 var numbers = "1234567890 ";
 var symbolCheck = lower + numbers;
+
+var colorfulScheme = true;
+
+$(document).ready(function(){
+	load_save();
+});
+
+function repeatString(r,number){
+	var j = "";
+	var n = 0;
+	while(n < number){
+		j += r;
+		n++;
+	};
+	return j;
+};
 
 function countW(){
 	var amountW = 0;
@@ -11,10 +27,11 @@ function countW(){
 	input = input.toLowerCase();
 	
 	var passage = "";
+	
 	var n = 0;
-	while(n < input.length){
-		if(!(symbolCheck.indexOf(input[n]))){
-			if(input[n] == "\n"){
+	for(n = 0; n < input.length; n++){
+		if(!(symbolCheck.indexOf(input[n]) > -1)){
+			if(input[n] === "\n"){
 				passage += " ";
 			}else{
 				passage += "";
@@ -22,7 +39,6 @@ function countW(){
 		}else{
 			passage += input[n];
 		};
-		n++;
 	};
 	
 	console.log(passage);
@@ -44,73 +60,142 @@ function countW(){
 	
 	console.log(words);
 	
-	n = 0;
-	while(n < words.length){
-		if(words[n] == ""){
-			words.splice(n,1);
-			amountW -= 1;
+	var new_words = [];
+	
+	for(n = 0;n < words.length;n++){
+		if(words[n] !== ""){
+			new_words.push(words[n]);
 		}else{
-			n++;
+			amountW -= 1;
 		};
 	};
 	
-	console.log(words);
+	console.log(new_words);
 	
 	var d = new Object;
-	for(var i in words){
-		d[i]= 0;
-	};
-	for(var x in words){
-		d[x] += 1;
+	for(n = 0; n < new_words.length; n++){
+		d[new_words[n]] = 0;
 	};
 	
+	for(n = 0; n < new_words.length; n++){
+		d[new_words[n]]++;
+	};
+	
+	console.log(d, amountW);
+	
+
 	var sortplace = [];
+	
 	for(var w in d){
 		sortplace.push([w, d[w]]);
 	};
-	sortplace.sort(function(a, b) {return a[1] - b[1]})
+	
+	console.log(sortplace)
+	
+	
+	sortplace.sort(function(a, b) {
+		return a[1] - b[1]
+	});
+	
+	console.log(sortplace);
+	
+	
 	var final = new Object;
-	for(var wordpair in sortplace){
-		final[wordpair[0]] = wordpair[1];
+	
+	for(n = 0; n < sortplace.length; n++){
+		final[sortplace[n][0]] = sortplace[n][1];
 	};
 	
-	console.log(final,amountW);
-	return [final, amountW];
+	console.log(final);
 	
+	var stat = "";
+	
+	for(var word in final){
+		var n = 30 - (word.length + final[word]);
+		var f = repeatString("-",n);
+		var b = repeatString("0",final[word]);
+		var string_amt = final[word].toString();
+		stat += word + ": " + f + " " + b + " (" + string_amt + ")" + "<br><br>";
+	};
+	
+	document.getElementById('amt_display').innerHTML = amountW;
+	
+	document.getElementById('stats').innerHTML = stat;
 	
 };
 
-function repeatString(r,number){
-	var j = "";
-	var n = 0;
-	while(n < number){
-		j += r;
-		n++;
-	};
-	return j;
+function cleartext(){
+	document.getElementById('textbox').value = "";
+	countW();
 }
 
-function return_stats_string(ordered_dict){
-	var stat = "";
-	for(var word in ordered_dict){
-		var n = 25 - word.length;
-		var f = repeatString("-",n);
-		var b = repeatString("[]",ordered_dict[word]);
-		var string_amt = ordered_dict[word];
-		string_amt = string_amt.toString;
-		stat += word + ": " + f + " " + b + " (" + string_amt + ")" + "\n";
-	};
-	return stat;
+function save(){
+	if(typeof(Storage) !== "undefined"){
+		var textSave = document.getElementById('textbox').value;
+		localStorage.wordCountSave = textSave;
+		localStorage.colorScheme = colorfulScheme;
+	}else{
+		alert("Your browser does not support this save feature.");
+	}
 };
 
-function runwordcount(){
-	var final_amt_list = countW();
-	var ordereddict = final_amt_list[0];
-	var amount = final_amt_list[1];
-	var stats_string = "Words: "+amount.toString()+"\n";
-	stats_string += return_stats_string(ordereddict);
-	$('#results').text(stats_string);
+function load_save(){
+	if(typeof(Storage) !== "undefined"){
+		if(localStorage.wordCountSave){
+			var loadSave = localStorage.wordCountSave;
+			document.getElementById('textbox').value = loadSave;
+			colorfulScheme = localStorage.colorScheme;
+		}else{
+			alert('You have no saves!');
+		}
+	}else{
+		alert("Your browser does not support this save feature.");
+	}
+	countW();
 };
+
+function wipeSave(){
+	if(typeof(Storage) !== "undefined"){
+		if(localStorage.wordCountSave){
+			if(confirm("Are you sure you want to wipe your save?")){
+				localStorage.removeItem('wordCountSave');
+			}
+		}else{
+			alert("You have no saves!");
+		}
+	}else{
+		alert("Your browser does not support this save feature.");
+	}
+};
+
+function colorSchemeToggle(){
+	if(colorfulScheme == true){
+		$('.jumbotron').css('background-color','#333333')
+		$('#title').css('color','white');
+		$('#navcontainer').css('background-color','#d6d6c2');
+		$('#wcnavbar').css('color','black');
+		$('.menubuttons').css('color','black');
+		$('#resultbox').css('background-color','#d9d9d9');
+		$('#results-head').css('color','black');
+		$('#word_display').css('color','black');
+		$('#color_toggle').text('Monochrome');
+		colorfulScheme = false;
+	}else{
+		$('.jumbotron').css('background-color','#33ff33')
+		$('#title').css('color','#330033');
+		$('#navcontainer').css('background-color','#80ffe5');
+		$('#wcnavbar').css('color','#000033');
+		$('.menubuttons').css('color','#000088');
+		$('#resultbox').css('background-color','#b3e6ff');
+		$('#results-head').css('color','#330033');
+		$('#word_display').css('color','#FF0033');
+		$('#color_toggle').text('Colorful');
+		colorfulScheme = true;
+	}
+};
+
+
+
 
 
 
