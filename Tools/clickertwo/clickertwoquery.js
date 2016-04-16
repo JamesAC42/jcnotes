0956ca586c10a4ffedd56e5697002055cbab56d6
JamesAC42 <jamescrovo2019@lschs.org>
2016-04-15 22:45:39 -0400

    Add DTM Game

---
+++ Tools/clickertwo/clickertwoquery.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$('.building').mousedown(function(){
+		$(this).css('border-left', '10px solid #6699ff');
+		$(this).css('border-bottom', '10px solid #6699ff');
+		$(this).css('border-top', '5px solid #00004d');
+		$(this).css('border-right', '5px solid #00004d');
+		$(this).css('background','linear-gradient(to right, #007acc , #002e4d)');
+	}).mouseup(function(){
+		$(this).css('border-left', '10px solid #00004d');
+		$(this).css('border-bottom', '10px solid #00004d');
+		$(this).css('border-top', '5px solid #6699ff');
+		$(this).css('border-right', '5px solid #6699ff');
+		$(this).css('background','linear-gradient(to right, #002e4d , #007acc)');
+	}).mouseenter(function(){
+		var name = $(this).attr('id');
+		var desc_name = '#' + name + '-description';
+		$(desc_name).slideDown('fast');
+	}).mouseleave(function(){
+		var name = $(this).attr('id');
+		var desc_name = "#" + name + '-description';
+		$(desc_name).slideUp('fast');
+	});
+	
+	$('#click-button').mousedown(function(){
+		$(this).css('margin-top','35px');
+	}).mouseup(function(){
+		$(this).css('margin-top','25px');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.building').fadeTo(100,0.25);
+});
+
+$(document).ready(function(){
+	$('.description').slideUp('fast');
+});
